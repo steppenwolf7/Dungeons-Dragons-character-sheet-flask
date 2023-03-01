5c93f7685f4a318351c6f3f2d8fd5612749bed4f
Rafal <rafaltomaszewsky@gmail.com>
2023-03-01 15:18:53 +0100

    Creating new table in schema.sql for character

--- schema.sql
+++ schema.sql
@@ -21,7 +21,15 @@ CREATE TABLE character (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
+  character_name TEXT NOT NULL,
+  character_class TEXT NOT NULL,
+  character_race TEXT NOT NULL,
+  character_strenght TEXT NOT NULL,
+  character_dexterity TEXT NOT NULL,
+  character_constitution TEXT NOT NULL,
+  character_intelligence TEXT NOT NULL,
+  character_wisdom TEXT NOT NULL,
+  character_charisma TEXT NOT NULL,
+  
   FOREIGN KEY (author_id) REFERENCES user (id)
 );
