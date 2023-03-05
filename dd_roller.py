ec8b9b8c674c97a94e7d036c684b3fe017208d22
Rafal <rafaltomaszewsky@gmail.com>
2023-03-05 22:49:45 +0100

    View is handling my form

--- dd_roller.py
+++ dd_roller.py
@@ -8,6 +8,8 @@ from .db import get_db
 
 bp = Blueprint('dd_roller', __name__)
 
+
+
 @bp.route('/roll')
 @login_required
 def roll():
@@ -20,21 +22,40 @@ def character():
         name = request.form.get('name')
         class_character = request.form.get('class_character')
         race = request.form.get('race')
-        strenght = request.form.get('get_data1')
-        dexterity = request.form.get('get_data2')
-        constitution = request.form.get('get_data3')
-        intelligence = request.form.get('get_data4')
-        wisdom = request.form.get('get_data5')
-        charisma = request.form.get('get_data6')
-        
+        strenght = request.form.get('strenght')
+        dexterity = request.form.get('dexterity')
+        constitution = request.form.get('constitution')
+        intelligence = request.form.get('intelligence')
+        wisdom = request.form.get('wisdom')
+        charisma = request.form.get('charisma')
+        #if not name:
+            #name = "Default Name"
         db = get_db()
         db.execute(
-                'INSERT INTO character (character_name, character_class, race, strenght, dexterity, constitution, intelligence, wisdom, charisma, author_id)' #columnes in data base 
-                ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                (name,class_character, race, strenght, dexterity, constitution, intelligence, wisdom, charisma, g.user['id']) 
-            )
+            'INSERT INTO character (character_name, character_class, race, strenght, dexterity, constitution, intelligence, wisdom, charisma, author_id)' #columnes in data base 
+            ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            (name, class_character, race, strenght, dexterity, constitution, intelligence, wisdom, charisma, g.user['id']) 
+        )
         db.commit()
         return redirect(url_for('roll'))
     
     return render_template('dd_roller/create_character.html')  
+
+
+
+def get_character(id, check_author=True):                                 #checking if author of post is logged in
+    character = get_db().execute(
+        'SELECT c.id, character_name, character_class, race, strenght, dexterity, constitution, intelligence, wisdom, charisma, author_id'
+        ' FROM character c JOIN user u ON c.author_id = u.id'
+        ' WHERE c.id = ?',
+        (id,)
+    ).fetchone()
+
+    if character is None:
+        abort(404, f"charcter id {id} doesn't exist.")
+
+    if check_author and character['author_id'] != g.user['id']:
+        abort(403)
+
+    return character
    
