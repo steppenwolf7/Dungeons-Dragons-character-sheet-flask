d0c77b27f6de9830547ed026d893b1e874e6b8dc
Rafal <rafaltomaszewsky@gmail.com>
2023-03-08 12:39:56 +0100

    style update

--- dd_roller.py
+++ dd_roller.py
@@ -57,16 +57,6 @@ def character():
 
 
 
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    get_post(id)
-    db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('blog.index'))    
-
-
 
 def get_character(id, check_author=True):                                 #checking if author of post is logged in
     character = get_db().execute(
